2b78add5b9750a6b5824882352967541e7dc583f
Mar4enkoOleg <rosstofor@gmail.com>
2021-05-21 02:32:52 +0300

    add mongo connection, add colors for console

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  console.log(`MongoDB connected: ${conn.connection.host}`.cyan)
+}
+
+export default connectDB
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import pkg from 'dotenv'
 import morgan from 'morgan'
-import logger from './middlewares/logger.js'
-import bootcamps from './routes/bootcamps.js'
-
+import colors from 'colors'
+colors.enable()
+// import logger from './middlewares/logger.js'
 pkg.config({ path: './config/config.env' })
+
+import connectDB from './config/db.js'
+connectDB()
 
 const PORT = process.env.PORT || 3002
 const app = express()
@@ -16,6 +19,18 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Routes
+import bootcamps from './routes/bootcamps.js'
 app.use('/api/v1/bootcamps', bootcamps)
 
-app.listen(PORT, () => console.info(`Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`))
+const server = app.listen(PORT, () => console.info(`Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`.cyan))
+
+// Handle unhandled promise rejections
+// Method change try/catch needs
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server & exit process
+  server.close(() => {
+    process.exit(1)
+  })
+})
