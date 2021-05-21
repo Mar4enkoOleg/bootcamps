13ed3c62db473e75331e1fae902dd3b1f39bd009
Mar4enkoOleg <rosstofor@gmail.com>
2021-05-21 03:51:33 +0300

    add bootcamp schema

---
+++ models/Bootcamp.js
@@ -0,0 +1,96 @@
+import { Schema, Model } from 'mongoose'
+
+const BootcampSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxLength: [50, 'Name can not be more than 50 letters'],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxLength: [500, 'Description can not be more than 500 letters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS',
+    ],
+  },
+  phone: {
+    type: String,
+    maxLength: [20, 'Phone can not be more than 20 characters'],
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add a valid email',
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
+  },
+  location: {
+    // GeoJSON point
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: ['Web development', 'Mobile development', 'UI/UX', 'Data science', 'Business', 'Other'],
+  },
+  avarageRaiting: {
+    type: Number,
+    min: [1, 'Raiting must be at least 1'],
+    max: [10, 'Raiting must can not be more than 10'],
+  },
+  avarageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistence: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export default Model('Bootcamp', BootcampSchema)
