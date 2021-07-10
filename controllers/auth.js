f3032d173f21608a915a94985789db447b251124
Mar4enkoOleg <rosstofor@gmail.com>
2021-07-10 06:19:15 +0300

    add forgot password and reset pass token

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,32 @@ export const login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res)
 })
 
+// @desc    Get current logged in user
+// @route   GET api/v1/auth/me
+// @access  Private
+export const getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+  res.status(200).json({ success: true, data: user })
+})
+
+// @desc    Forgot password
+// @route   GET api/v1/auth/forgotpassword
+// @access  Public
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email })
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404))
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken()
+
+  await user.save({ validateBeforeSave: false })
+
+  res.status(200).json({ success: true, data: user })
+})
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
@@ -63,11 +89,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     token,
   })
 }
-
-// @desc    Get current logged in user
-// @route   GET api/v1/auth.me
-// @access  Private
-export const getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id)
-  res.status(200).json({ success: true, data: user })
-})
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, register, getMe } from '../controllers/auth.js'
+import { login, register, getMe, forgotPassword } from '../controllers/auth.js'
 import { protect } from '../middlewares/auth.js'
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/me').get(protect, getMe)
+router.route('/forgotpassword').post(forgotPassword)
 
 export default router
