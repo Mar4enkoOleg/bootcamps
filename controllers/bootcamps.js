f5c8117687181239e7a6a10997f77aa789965501
Mar4enkoOleg <rosstofor@gmail.com>
2021-07-10 04:46:37 +0300

    add validation relation owner user - bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -50,13 +50,21 @@ export const createBootcamp = asyncHandler(async (req, res, next) => {
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 export const updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  })
+  let bootcamp = await Bootcamp.findById(req.params.id)
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamp with id: ${req.params.id} not found`, 404))
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this bootcamp`, 400))
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
   res.status(200).json({ success: true, data: bootcamp })
 })
 
@@ -68,6 +76,12 @@ export const deleteBootcamp = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamp with id: ${req.params.id} not found`, 404))
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete this bootcamp`, 401))
+  }
+
   bootcamp.remove()
   res.status(200).json({ success: true, data: {} })
 })
@@ -106,6 +120,12 @@ export const bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404))
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete this bootcamp`, 401))
+  }
+
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400))
   }
